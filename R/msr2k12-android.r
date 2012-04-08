fdcd6472a7cb3aa9a02799965c2e09a26da02958
Francisco Rocha <rocha.francisco.a@gmail.com>
2012-04-08 17:53:27 +0200

    Add to paper initial results. Bibliography extended.

--- R/msr2k12-android.r
+++ R/msr2k12-android.r
@@ -1,4 +1,11 @@
-
+# Function meanWindows
+# This function splits a vector into segments of a given window size, and returns
+# a vector containing the means of each window
+# Params:
+#  v: vector which want to be "windowized"
+#  windowSize: integer containing the size of the window
+# Return:
+#  A vector containing the windows means
 meanWindows <- function(v, windowSize)
 {
   windowsNumber = as.integer(length(v)/windowSize)
@@ -14,12 +21,41 @@ meanWindows <- function(v, windowSize)
   means
 }
 
+# Function medianMaker2
+# This function calculates the median values from the given vector grouping
+# the values in function of the stars of the vector x
+# Params:
+#  v: vector containing the values
+#  x: vector containing the stars rankings of each value from v with the same index
+# Return:
+#  A vector containing the median values, each index for a star value
+medianMaker2 <- function(v, x)
+{
+  starsLimit = max(x)
+  medians = 0:starsLimit
+  
+  for (star in 0:starsLimit)
+  {
+    medians[star] = median(v[x==star])
+  }
+  
+  medians
+}
+
+# Function meanMaker2
+# This function calculates the mean values from the given vector grouping
+# the values in function of the stars of the vector x
+# Params:
+#  v: vector containing the values
+#  x: vector containing the stars rankings of each value from v with the same index
+# Return:
+#  A vector containing the mean values, each index for a star value
 meanMaker2 <- function(v, x)
 {
   starsLimit = max(x)
-  means = 1:starsLimit
+  means = 0:starsLimit
   
-  for (star in 1:starsLimit)
+  for (star in 0:starsLimit)
   {
     means[star] = mean(v[x==star])
   }
@@ -27,12 +63,20 @@ meanMaker2 <- function(v, x)
   means
 }
 
-
+# Function meanMaker
+# This function calculates the mean values from the given vector grouping
+# the values in function of the stars of the vector x
+# Params:
+#  v: vector containing the values
+#  stars: vector containing the stars rankings of each value from v with the same index
+#  starsLimit: limit of the maximum star value to be used (to improve calculation time)
+# Return:
+#  A vector containing the mean values, each index for a star value
 meanMaker <- function(v, stars, starsLimit=max(stars))
 {
-  means = 1:starsLimit
+  means = 0:starsLimit
   
-  for (star in 1:starsLimit)
+  for (star in 0:starsLimit)
   {
     counter = 0
     means[star] = 0
@@ -52,59 +96,103 @@ meanMaker <- function(v, stars, starsLimit=max(stars))
 
 library("lattice")
 
-setwd("~/mswl/mswl/dynamicsls/paper/")
-android = read.table("./msr2k12-android/bug_stars_duration_clean.txt", header=T)
+# Setting the working path
+setwd("~/mswl/17.Dynamics-of-Libre-Software-Communities/MSR-2012-Android-stars/R/")
+
+# Read the data (relative path to the working path)
+android = read.table("~/workspace/msr2k12-android/bug_stars_duration_clean.txt", header=T)
 attach(android)
 
-mean(Stars)
-sd(Stars)
+# Stars variable
+print("Stars")
+print(summary(Stars))
+print(paste("Standard deviation", sd(Stars)))
 boxplot(Stars, main="Stars boxplot", ylab="Stars")
 barplot(Stars, main="Stars barplot", xlab="Bugs", ylab="Stars")
 
-mean(Duration)
-sd(Duration)
+# Duration variable
+print("Duration")
+print(summary(Duration))
+print(paste("Standard deviation", sd(Duration)))
 boxplot(Duration/(60*60*24*365), main="Durations boxplot", ylab="Durations (years)")
 barplot(Duration/(60*60*24*365), main="Durations barplot", xlab="Bugs", ylab="Durations (years)")
 
+# Type variable
+print("Type")
+print(summary(Type))
 TypeInt = as.integer(Type)
-mean(TypeInt)
-sd(TypeInt)
+print(summary(TypeInt))
+print(paste("Standard deviation", sd(TypeInt)))
 boxplot(TypeInt, main="Types boxplot", ylab="Types")
 barplot(TypeInt, main="Types barplot", xlab="Bugs", ylab="Types")
 plot(TypeInt, main="Types", xlab="Bugs", ylab="Types")
 
+# Type priority
+print("Priority")
+print(summary(Priority))
 PriorityInt = as.integer(Priority)
-mean(PriorityInt)
-sd(PriorityInt)
+print(summary(PriorityInt))
+print(paste("Standard deviation", sd(PriorityInt)))
 boxplot(PriorityInt, main="Priorities boxplot", ylab="Priorities")
 barplot(PriorityInt, main="Priorities barplot", xlab="Bugs", ylab="Priorities")
-plot(PriorityInt, main="Priorities", xlab="Bugs", ylab="Priorities")
+plot(PriorityInt, main="Priorities", xlab="Stars", ylab="Priorities")
+
 stripplot(Priority~Stars, main="Priorities of the bugs in function of their stars", xlab="Stars", ylab="Priorities")
 
-#plot(Stars, Duration)
+# Representations
+# Duration vs Stars
+plot(jitter(Stars, factor=5), jitter(Duration/(60*60*24*365), factor=2), col=rgb(0, 100, 0, 50, maxColorValue=255), pch=16, main="Time to solve a bug vs stars"); grid()
+plot(jitter(Stars, factor=5), jitter(Duration/(60*60*24*365), factor=2), col=rgb(0, 100, 0, 50, maxColorValue=255), pch=16, log="xy", main="Time to solve a bug vs stars (log)"); grid()
 
-timemeans = meanMaker2(Duration, Stars)
+# Calculation of the medians of the durations in function of the stars
+timeMedians = medianMaker2(Duration, Stars)
+x = 0:max(Stars)
+y = timeMedians/(60*60*24*365)
 
-x = 1:max(Stars)
-y = timemeans/(60*60*24*365)
+# Plot of the medians of the durations in function of the stars
+plot(x, y, main="Median time to solve a bug vs stars", xlab="Stars", xlim=c(1,7500), ylab="Mean time to be solved (years)"); grid()
+plot(log10(x), log10(y), main="Median time to solve a bug vs stars (logarithmic)", xlab="Stars", ylab="Mean time to be solved (years)"); grid()
 
-plot(x, y, main="Mean time to solve a bug vs stars", xlab="Stars", xlim=c(1,7500), ylab="Mean time to be solved (years)", ylim=c(1,4)); grid()
-plot(log10(x), log10(y), main="Mean time to solve a bug vs stars (logarithmic)", xlab="Stars", ylab="Mean time to be solved (years)", ylim=c(-4,1)); grid()
+# Calculation of the means of the durations in function of the stars
+timeMeans = meanMaker2(Duration, Stars)
+x = 0:max(Stars)
+y = timeMeans/(60*60*24*365)
 
-timemeansWindows = meanWindows(y, 100)
-barplot(timemeansWindows, ylab="Mean time to be solved (years)", main="Mean time to solve a bug vs stars (groups 100 stars)")
-plot(seq(1, length(timemeans), 100), timemeansWindows, ylab="Mean time to be solved (years)", main="Mean time to solve a bug vs stars (groups 100 stars)")
+# Plot of the means of the durations in function of the stars
+plot(x, y, main="Mean time to solve a bug vs stars", xlab="Stars", xlim=c(1,7500), ylab="Mean time to be solved (years)"); grid()
+plot(log10(x), log10(y), main="Mean time to solve a bug vs stars (logarithmic)", xlab="Stars", ylab="Mean time to be solved (years)"); grid()
 
+# Calculation of the means of the durations in windows in function of the stars
+timeMeansWindows = meanWindows(y, 100)
+
+# Plot of the means of the durations in windows in function of the stars
+barplot(timeMeansWindows, ylab="Mean time to be solved (years)", main="Mean time to solve a bug vs stars (groups 100 stars)")
+plot(seq(1, length(timeMeans), 100), timeMeansWindows, ylab="Mean time to be solved (years)", main="Mean time to solve a bug vs stars (groups 100 stars)")
+
+# Calculation of the medians of the priorities in function of the stars
 prioridades = as.integer(rev(Priority))
-priomeans = meanMaker2(prioridades, Stars)
+priorityMedians = medianMaker2(prioridades, Stars)
+x = 0:max(Stars)
+y = priorityMedians
 
-x = 1:max(Stars)
-y = priomeans
+# Plot of the medians of the priorities in function of the stars
+plot(x, y, main="Medians priority of a bug vs stars", xlab="Stars", xlim=c(1,7500), ylab="Priority"); grid()
+plot(log10(x), log10(y), main="Medians priority of a bug vs stars (logarithmic)", xlab="Stars", ylab="Priority"); grid()
 
-plot(x, y, main="Mean priority of a bug vs stars", xlab="Stars", xlim=c(1,7500), ylab="Priority", ylim=c(0,5)); grid()
+# Calculation of the means of the priorities in function of the stars
+priorityMeans = meanMaker2(prioridades, Stars)
+x = 0:max(Stars)
+y = priorityMeans
+
+# Plot of the means of the priorities in function of the stars
+plot(x, y, main="Mean priority of a bug vs stars", xlab="Stars", xlim=c(1,7500), ylab="Priority"); grid()
 plot(log10(x), log10(y), main="Mean priority of a bug vs stars (logarithmic)", xlab="Stars", ylab="Priority"); grid()
-priomeansWindows = meanWindows(priomeans, 100)
-barplot(priomeansWindows, ylab="Priority", ylim=c(0,5), main="Mean priority of a bug vs stars (groups 100 stars)")
-plot(seq(1, length(timemeans), 100), priomeansWindows, ylab="Priority", main="Mean priority of a bug vs stars (groups 100 stars)")
 
-#plot(priomeans)
+# Calcualation of the means of the priorities in windows in function of the stars
+priomeansWindows = meanWindows(priorityMeans, 100)
+
+# Plot of the means of the priorities in windows in function of the stars
+barplot(priomeansWindows, ylab="Priority", main="Mean priority of a bug vs stars (groups 100 stars)")
+plot(seq(1, length(timeMeans), 100), priomeansWindows, ylab="Priority", main="Mean priority of a bug vs stars (groups 100 stars)")
+
+detach(android)
